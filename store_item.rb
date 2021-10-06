47d2f0335676a2f06378edff3a72eeae6b534e97
Devorah Ortega <devorahb.ortega@gmail.com>
2021-10-06 14:28:35 -0400

    Changes the initialize to accept input_options and cleans up space

--- store_item.rb
+++ store_item.rb
@@ -1,39 +1,20 @@
 #inventory = [{ :item => "doorknob", :color => "silver", :price => 56.44 }, { :item => "submarine", :color => "yellow", :price => 12000.00 }, { :item => "hat", :color => "red", :price => 12.00 }]
 #inventory2 = [{ item: "doorknob", color: "silver", price: 56.44 }, { item: "submarine", color: "yellow", price: 12000.00 }, { item: "hat", color: "red", price: 12.00 }]
 
-#p inventory
-#p inventory2
-
 class Item
   attr_reader :name, :color, :price
   attr_writer :price
 
-  def initialize(name, color, price)
-    @name = name
-    @color = color
-    @price = price
+  def initialize(input_options)
+    @name = input_options[:name]
+    @color = input_options[:color]
+    @price = input_options[:price]
   end
-
-  #def change_price(price)
-  #  @price = price
-  #end
-
-  #def name
-  # @name
-  #end
-
-  #def color
-  #  @color
-  #end
-
-  #def price
-  #  @price
-  #end
 end
 
-item1 = Item.new("doorknob", "silver", 56.44)
-item2 = Item.new("submarine", "yellow", 12000.00)
-item3 = Item.new("hat", "red", 12.00)
+item1 = Item.new{:item => "doorknob", :color => "silver", :price => 56.44}
+item2 = Item.new[{ :item => "submarine", :color => "yellow", :price => 12000.00 }]
+item3 = Item.new[{ :item => "hat", :color => "red", :price => 12.00 }]
 
 p item1
 p item2
