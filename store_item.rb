2fed9411f1b31d7cc93902f3239cd231988f0488
Devorah Ortega <devorahb.ortega@gmail.com>
2021-10-06 14:52:08 -0400

    Creates class Vehicle and runs the code

--- store_item.rb
+++ store_item.rb
@@ -12,9 +12,9 @@ class Item
   end
 end
 
-item1 = Item.new{:item => "doorknob", :color => "silver", :price => 56.44}
-item2 = Item.new[{ :item => "submarine", :color => "yellow", :price => 12000.00 }]
-item3 = Item.new[{ :item => "hat", :color => "red", :price => 12.00 }]
+item1 = Item.new(:item => "doorknob", :color => "silver", :price => 56.44)
+item2 = Item.new(:item => "submarine", :color => "yellow", :price => 12000.00)
+item3 = Item.new(:item => "hat", :color => "red", :price => 12.00)
 
 p item1
 p item2
